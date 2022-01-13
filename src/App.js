39ff2bd3f50cfecd771dae59516308d55d808e05
Andrew Lawson <tc6899b@gmail.com>
2022-01-13 16:57:00 +0000

    removing unused imports and fixing path

--- src/App.js
+++ src/App.js
@@ -1,37 +1,30 @@
-import { useState } from 'react'
-import { BrowserRouter, Route, Link } from 'react-router-dom'
+import { BrowserRouter, Route } from "react-router-dom";
 
-import './style.css'
+import "./style.css";
 
-import FullBlueButton from './FullBlueButton'
-import RedButton from './RedButton'
-import Footer from './Footer'
-import Student from './Student'
-import StudentList from './StudentList'
-import Counter from './Counter'
-import Home from './Home'
-import StudentsProvider from './context/Provider'
-import CreateStudentForm from './CreateStudentForm'
+import FullBlueButton from "./FullBlueButton";
+import RedButton from "./RedButton";
+import Footer from "./Footer";
+import Student from "./Student";
+import Home from "./Home";
+import StudentsProvider from "./context/students/Provider";
+import CreateStudentForm from "./CreateStudentForm";
 
 function App() {
   return (
     <BrowserRouter>
       {/* if (path === '/red') return <RedButton /> */}
-      <Route path='/red' exact component={RedButton} />
+      <Route path="/red" exact component={RedButton} />
 
       {/* if (path === '/blue') return <FullBlueButton /> */}
-      <Route
-        path='/blue' exact component={FullBlueButton}
-      />
+      <Route path="/blue" exact component={FullBlueButton} />
 
       <StudentsProvider>
         <CreateStudentForm />
 
-        <Route
-          path='/student/:studentName/:id?' exact component={Student}
-        />
+        <Route path="/student/:studentName/:id?" exact component={Student} />
 
-        <Route path='/' exact component={Home} />
+        <Route path="/" exact component={Home} />
       </StudentsProvider>
 
       <Footer />
